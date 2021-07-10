8d88e5e97fd2c9b094bf4326e2a2599670f57422
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2021-07-10 16:32:29 +0530

    Add slo-mo ability for the player

--- chronozine/src/levels/Level.gd
+++ chronozine/src/levels/Level.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 
 const LASER_SCN := preload("res://src/turrets/Laser.tscn")
+const SLO_MO := 0.125
 
 onready var lasers: Node2D = $Lasers
 
@@ -10,6 +11,14 @@ onready var lasers: Node2D = $Lasers
 func _ready() -> void:
 	for gun in get_tree().get_nodes_in_group("gun"):
 		gun.connect("laser_fired", self, "_on_laser_fired")
+
+
+func _on_Player_started_slo_mo() -> void:
+	Engine.time_scale = SLO_MO
+
+
+func _on_Player_stopped_slo_mo() -> void:
+	Engine.time_scale = 1.0
 
 
 func _on_laser_fired(pos: Vector2, dir: Vector2) -> void:
--- chronozine/src/player/Player.gd
+++ chronozine/src/player/Player.gd
@@ -2,6 +2,10 @@ class_name Player
 extends KinematicBody2D
 
 
+signal started_slo_mo
+signal stopped_slo_mo
+
+
 const MAX_SPEED := 800
 const ACCELERATION := 0.016
 const DECELERATION := 0.008
@@ -10,10 +14,21 @@ const HANDLING := 0.2
 var velocity := Vector2()
 
 onready var exhaust: Node2D = $Exhaust
+onready var wave: Particles2D = $Wave
 
 
 func hurt(damage: int) -> void:
 	print("HURT")
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("slo_mo"):
+		wave.emitting = true
+		emit_signal("started_slo_mo")
+
+	if Input.is_action_just_released("slo_mo"):
+		wave.emitting = false
+		emit_signal("stopped_slo_mo")
 
 
 func _physics_process(_dt: float) -> void:
