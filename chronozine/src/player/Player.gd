479b1a43c9f177a2beff705197aca9e1811d2776
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2021-07-10 15:03:27 +0530

    Create scenes for the player and an empty level

---
+++ chronozine/src/player/Player.gd
@@ -0,0 +1,40 @@
+class_name Player
+extends KinematicBody2D
+
+
+const MAX_SPEED := 800
+const ACCELERATION := 0.016
+const DECELERATION := 0.008
+const HANDLING := 0.2
+
+var velocity := Vector2()
+
+onready var exhaust: Node2D = $Exhaust
+
+
+func _physics_process(_dt: float) -> void:
+	var target_dir := _direction_input()
+	rotation = lerp_angle(rotation, target_dir.angle(), HANDLING)
+
+	if Input.is_action_pressed("accelerate"):
+		var target_velocity := target_dir * MAX_SPEED
+		velocity = lerp(velocity, target_velocity, ACCELERATION)
+		exhaust.show()
+	else:
+		var target_velocity := Vector2()
+		velocity = lerp(velocity, target_velocity, DECELERATION)
+		exhaust.hide()
+
+	velocity = move_and_slide(velocity)
+
+
+func _direction_input() -> Vector2:
+	var strength := Vector2(
+		Input.get_action_strength("point_rt") - Input.get_action_strength("point_lt"),
+		Input.get_action_strength("point_dn") - Input.get_action_strength("point_up")
+	)
+
+	if strength:
+		return strength.normalized()
+	else:
+		return Vector2(cos(rotation), sin(rotation))
